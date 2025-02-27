b2151d3b40ad3f4db648df5b304eff8749285dd1
Simbad <43704227+simbadmorehod@users.noreply.github.com>
2025-02-27 12:20:38 +0530

    Update update_embeddings.py

--- update_embeddings.py
+++ update_embeddings.py
@@ -13,7 +13,7 @@ from models import Messages, MessageEmbeddings
 
 # Конфигурация
 EMBEDDING_DIM = 1024  # Для bge-small-en-v1.5
-BATCH_SIZE = 10000  # Оптимальный размер батча для GPU (подберите под вашу видеокарту)
+BATCH_SIZE = 5000  # Оптимальный размер батча для GPU (подберите под вашу видеокарту)
 MAX_TEXT_LENGTH = 500  # Максимальная длина текста
 
 # Настройка логирования
